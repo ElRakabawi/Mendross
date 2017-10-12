bac5c21f401060b2ba4df60e335e00bf980d493c
Muhammed <mselrakabawi@gmail.com>
2017-10-12 08:51:13 +0200

    dihybrid crossing

--- core.pl
+++ core.pl
@@ -15,7 +15,7 @@ my $len_two = length($p_two); #length of Parent two genotype
 #Number of genes input and verification
 while($genes > 3){
   if($genes > 3){
-    print "Genes number must be between 1 and 3\n";
+    print color("RED"), "Genes number must be between 1 and 3\n", color("RESET");
   }
   print "Enter number of genes:";
    $genes = <STDIN>;
@@ -27,9 +27,9 @@ my $alleles = $genes*2; #Number of alleles
 #Parent one genotype input and verification
 while($len_one != $alleles){
   if($len_one != $alleles){
-    print "Genotype must contain $alleles alleles\n";
+    print color("RED"), "Genotype must contain $alleles alleles\n", color("RESET");
   }
-  print "Enter parent(1) genotype:";
+  print "Enter parent(1) genotype:\n";
   $p_one = <STDIN>;
   chomp $p_one;
 
@@ -39,18 +39,22 @@ while($len_one != $alleles){
 #Parent two genotype input and verification
 while($len_two != $alleles){
   if($len_two != $alleles){
-    print "Genotype must contain $alleles alleles\n";
+    print color("RED"), "Genotype must contain $alleles alleles\n", color("RESET");
   }
-  print "Enter parent(2) genotype:";
+  print "Enter parent(2) genotype:\n";
   $p_two = <STDIN>;
   chomp $p_two;
 
   $len_two = length($p_two);
 }
 
-my @POG = ();
-my @PTG = ();
+my @POG = (); #Parent One Gametes
+my @PTG = (); #Parent Two Gametes
+my @POS = (); #Parent one step
+my @PTS = (); #Parent two step
 
+
+#Computation of monohybrid crossing
 if($genes == 1){
   for(my $i=0; $i<$alleles; $i++){
     $POG[$i] = substr($p_one,$i,1);
@@ -60,14 +64,112 @@ if($genes == 1){
 
   my $rows = [
       #header rows
-      ["GA","$POG[0]", "$POG[1]",],
+      ["  ","$POG[0]", "$POG[1]",],
       #rows
       [ "$PTG[0]", $PTG[0].$POG[0], $PTG[0].$POG[1] ],
       [ "$PTG[1]", $PTG[1].$POG[0], $PTG[1].$POG[1] ],
 
     ];
 
+    print color("GREEN");
+    print "The punett square for this monohybrid cross: \n";
     print generate_table(rows => $rows, header_row => 1, separate_rows => 1);
     print "\n";
+    print color("RESET");
 
+}
+
+#Computation of dihybrid crossing
+elsif($genes == 2) {
+  for(my $i=0; $i<$alleles; $i++){
+    $POS[$i] = substr($p_one,$i,1);
+    $PTS[$i] = substr($p_two,$i,1);
+  }
+
+  my $m = 0;
+  for(my $i=0; $i<$alleles; $i+=2){
+
+    $POG[$i][0] = $POS[$m];
+    $POG[$i][1] = $POS[2];
+    $POG[$i+1][0] = $POS[$m];
+    $POG[$i+1][1] = $POS[3];
+    $m++;
+  }
+
+  my $n = 0;
+  for(my $i=0; $i<$alleles; $i+=2){
+
+    $PTG[$i][0] = $PTS[$n];
+    $PTG[$i][1] = $PTS[2];
+    $PTG[$i+1][0] = $PTS[$n];
+    $PTG[$i+1][1] = $PTS[3];
+    $n++;
+  }
+
+
+  my $rows = [
+      #header rows
+            ["  ","$POG[0][0]"."$POG[0][1]", "$POG[1][0]"."$POG[1][1]","$POG[2][0]"."$POG[2][1]", "$POG[3][0]"."$POG[3][1]",],
+      #rows
+      [ "$PTG[0][0]"."$PTG[0][1]", $PTG[0][0].$POG[0][0].$PTG[0][1].$POG[0][1], $PTG[0][0].$POG[1][0].$PTG[0][1].$POG[1][1],
+      $PTG[0][0].$POG[2][0].$PTG[0][1].$POG[2][1], $PTG[0][0].$POG[3][0].$PTG[0][1].$POG[3][1] ], #row 1
+      [ "$PTG[1][0]"."$PTG[1][1]", $PTG[1][0].$POG[0][0].$PTG[1][1].$POG[0][1], $PTG[1][0].$POG[1][0].$PTG[1][1].$POG[1][1],
+      $PTG[1][0].$POG[2][0].$PTG[1][1].$POG[2][1], $PTG[1][0].$POG[3][0].$PTG[1][1].$POG[3][1] ], #row 2
+      [ "$PTG[2][0]"."$PTG[2][1]", $PTG[2][0].$POG[0][0].$PTG[2][1].$POG[0][1], $PTG[2][0].$POG[1][0].$PTG[2][1].$POG[1][1],
+      $PTG[2][0].$POG[2][0].$PTG[2][1].$POG[2][1], $PTG[2][0].$POG[3][0].$PTG[2][1].$POG[3][1] ], #row 3
+      [ "$PTG[3][0]"."$PTG[3][1]", $PTG[3][0].$POG[0][0].$PTG[3][1].$POG[0][1], $PTG[3][0].$POG[1][0].$PTG[3][1].$POG[1][1],
+      $PTG[3][0].$POG[2][0].$PTG[3][1].$POG[2][1], $PTG[3][0].$POG[3][0].$PTG[3][1].$POG[3][1] ], #row 4
+
+    ];
+
+    print color("GREEN");
+    print "The punett square for this dihybrid cross: \n";
+    print generate_table(rows => $rows, header_row => 1, separate_rows => 1);
+    print "\n";
+    print color("RESET");
+}
+
+elsif($genes == 3) {
+  for(my $i=0; $i<$alleles; $i++){
+    $POS[$i] = substr($p_one,$i,1);
+    $PTS[$i] = substr($p_two,$i,1);
+  }
+
+  my $m = 0;
+  for(my $i=0; $i<$alleles; $i+=2){
+
+    $POG[$i][0] = $POS[$m];
+    $POG[$i][1] = $POS[2];
+    $POG[$i+1][0] = $POS[$m];
+    $POG[$i+1][1] = $POS[3];
+    $m++;
+  }
+
+  my $n = 0;
+  for(my $i=0; $i<$alleles; $i+=2){
+
+    $PTG[$i][0] = $PTS[$n];
+    $PTG[$i][1] = $PTS[2];
+    $PTG[$i+1][0] = $PTS[$n];
+    $PTG[$i+1][1] = $PTS[3];
+    $n++;
+  }
+
+
+  my $rows = [
+      #header rows
+            ["  ","$POG[0][0]"."$POG[0][1]", "$POG[1][0]"."$POG[1][1]","$POG[2][0]"."$POG[2][1]", "$POG[3][0]"."$POG[3][1]",],
+      #rows
+      [ "$PTG[0][0]"."$PTG[0][1]", $PTG[0][0].$POG[0][0].$PTG[0][1].$POG[0][1], $PTG[0][0].$POG[1][0].$PTG[0][1].$POG[1][1], $PTG[0][0].$POG[2][0].$PTG[0][1].$POG[2][1], $PTG[0][0].$POG[3][0].$PTG[0][1].$POG[3][1] ],
+      [ "$PTG[1][0]"."$PTG[1][1]", $PTG[1][0].$POG[0][0].$PTG[1][1].$POG[0][1], $PTG[1][0].$POG[1][0].$PTG[1][1].$POG[1][1], $PTG[1][0].$POG[2][0].$PTG[1][1].$POG[2][1], $PTG[1][0].$POG[3][0].$PTG[1][1].$POG[3][1] ],
+      [ "$PTG[2][0]"."$PTG[2][1]", $PTG[2][0].$POG[0][0].$PTG[2][1].$POG[0][1], $PTG[2][0].$POG[1][0].$PTG[2][1].$POG[1][1], $PTG[2][0].$POG[2][0].$PTG[2][1].$POG[2][1], $PTG[2][0].$POG[3][0].$PTG[2][1].$POG[3][1] ],
+      [ "$PTG[3][0]"."$PTG[3][1]", $PTG[3][0].$POG[0][0].$PTG[3][1].$POG[0][1], $PTG[3][0].$POG[1][0].$PTG[3][1].$POG[1][1], $PTG[3][0].$POG[2][0].$PTG[3][1].$POG[2][1], $PTG[3][0].$POG[3][0].$PTG[3][1].$POG[3][1] ],
+
+    ];
+
+    print color("GREEN");
+    print "The punett square for this dihybrid cross: \n";
+    print generate_table(rows => $rows, header_row => 1, separate_rows => 1);
+    print "\n";
+    print color("RESET");
 }
